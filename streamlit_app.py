5b985dc6e79fdff70ca2e79b683673050e0c5be8
PJP-Pooch <phillip@poochandmutt.com>
2025-05-02 12:16:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,19 +147,9 @@ if "account" in st.session_state:
         site_urls = [site["siteUrl"] for site in site_list["siteEntry"]]
 
         with st.form("gsc_form"):
-            
-            if "last_selected_site" not in st.session_state:
-                st.session_state["last_selected_site"] = site_urls[0]
-    
-                st.selectbox(
-                    "🌐 Select GSC Property",
-                    site_urls,
-                    key="last_selected_site"
-                )
-            
-                selected_site = st.session_state["last_selected_site"]
-                timescale = st.selectbox("Date range", ["Last 7 days", "Last 28 days", "Last 3 months", "Last 12 months"])
-                submit_gsc = st.form_submit_button("📊 Fetch GSC Data")
+            selected_site = st.selectbox("🌐 Select GSC Property", site_urls)
+            timescale = st.selectbox("Date range", ["Last 7 days", "Last 28 days", "Last 3 months", "Last 12 months"])
+            submit_gsc = st.form_submit_button("📊 Fetch GSC Data")
 
         if submit_gsc:
             days_map = {"Last 7 days": -7, "Last 28 days": -28, "Last 3 months": -90, "Last 12 months": -365}
